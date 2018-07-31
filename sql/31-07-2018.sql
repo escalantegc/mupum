2ad1e45a148412517731ca36ad5545869b73aa97
Gustavo Escalante <gustavo.escalante@campus.unam.edu.ar>
2018-07-31 08:46:57 -0300

    triggers creacion de deuda consumo financiado

---
+++ sql/31-07-2018.sql
@@ -0,0 +1,45 @@
+ALTER TABLE public.detalle_consumo_financiado ADD COLUMN fecha date;
+
+-- Function: public.generar_numeros_talonario()
+
+-- DROP FUNCTION public.generar_numeros_talonario();
+
+CREATE OR REPLACE FUNCTION public.generar_deuda_consumo_financiado()
+  RETURNS trigger AS
+$BODY$
+DECLARE
+    inicio integer := 0;
+    fin integer := New.cantidad_cuotas;
+    monto double precision := new.total / fin;
+    fecha_cuota date:=New.fecha;
+BEGIN
+    
+	 LOOP
+	 
+	 EXIT WHEN inicio > fin ;
+		INSERT INTO public.detalle_consumo_financiado( idconsumo_financiado, nro_cuota, mes, anio, monto, fecha)
+		VALUES ( New.idconsumo_financiado, inicio+1, extract(month from (fecha_cuota + (inicio||' months')::interval)), extract(YEAR from (fecha_cuota + (inicio||' months')::interval)), monto, (fecha_cuota + (inicio||' months')::interval)::date);
+		inicio:=inicio+1;
+		
+		
+	END LOOP;
+   RETURN null;
+END
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION public.generar_deuda_consumo_financiado()
+  OWNER TO postgres;
+
+
+-- Trigger: generar_numeros_talonario on public.talonario_bono
+
+-- DROP TRIGGER generar_numeros_talonario ON public.talonario_bono;
+
+CREATE TRIGGER generar_deuda_consumo_financiado
+  AFTER INSERT
+  ON public.consumo_financiado
+  FOR EACH ROW
+  EXECUTE PROCEDURE public.generar_deuda_consumo_financiado();
+
+  
