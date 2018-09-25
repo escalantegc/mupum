496eb71e6bbba4b24dd14a3b8f3dfbde9c3b7c68
Gustavo Escalante <escalantegc@gmail.com>
2018-09-25 17:16:09 -0300

    a

--- metadatos/componentes/toba_datos_tabla/dump_106000062.sql
+++ metadatos/componentes/toba_datos_tabla/dump_106000062.sql
@@ -153,4 +153,18 @@ INSERT INTO apex_objeto_db_registros_col (objeto_proyecto, objeto, col_id, colum
 	'0', --externa
 	'detalle_pago'  --tabla
 );
+INSERT INTO apex_objeto_db_registros_col (objeto_proyecto, objeto, col_id, columna, tipo, pk, secuencia, largo, no_nulo, no_nulo_db, externa, tabla) VALUES (
+	'mupum', --objeto_proyecto
+	'106000062', --objeto
+	'106000432', --col_id
+	'fecha', --columna
+	'F', --tipo
+	'0', --pk
+	'', --secuencia
+	NULL, --largo
+	NULL, --no_nulo
+	'0', --no_nulo_db
+	'0', --externa
+	'detalle_pago'  --tabla
+);
 --- FIN Grupo de desarrollo 106
--- php/solicitudes/ei_frm_detalle_pago.php
+++ php/solicitudes/ei_frm_detalle_pago.php
@@ -25,6 +25,15 @@ class ei_frm_detalle_pago extends mupum_ei_formulario_ml
 				this.ef('monto').ir_a_fila(fila).resetear_estado();
 			}
 		}
+		{$this->objeto_js}.crear_fila_orig = {$this->objeto_js}.crear_fila; 
+		{$this->objeto_js}.crear_fila       = function() {
+			
+			var hoy = new Date();
+			var hoy_texto = hoy.getDate() + '/' + (hoy.getMonth() +1)  + '/' + hoy.getFullYear();
+			id_fila = this.crear_fila_orig();
+			this.ef('fecha').ir_a_fila(id_fila).set_solo_lectura(true);
+			this.ef('fecha').ir_a_fila(id_fila).set_estado(hoy_texto);
+		}
 		";
 	}
 
