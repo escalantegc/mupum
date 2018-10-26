1a1ccd2e294bb3a79d2d9b4f8100410a5e06c55a
Gustavo Escalante <escalantegc@gmail.com>
2018-10-26 19:09:47 -0300

    a

--- metadatos/componentes/toba_item/dump_106000073.sql
+++ metadatos/componentes/toba_item/dump_106000073.sql
@@ -1,5 +1,5 @@
 ------------------------------------------------------------
---[106000073]--  Administrar talonario 
+--[106000073]--  Vender Bono 
 ------------------------------------------------------------
 
 ------------------------------------------------------------
@@ -23,7 +23,7 @@ INSERT INTO apex_item (item_id, proyecto, item, padre_id, padre_proyecto, padre,
 	NULL, --actividad_buffer
 	NULL, --actividad_patron_proyecto
 	NULL, --actividad_patron
-	'Administrar talonario', --nombre
+	'Vender Bono', --nombre
 	NULL, --descripcion
 	'28', --punto_montaje
 	NULL, --actividad_accion
--- php/solicitudes/ci_administrar_bonos_colaboracion.php
+++ php/solicitudes/ci_administrar_bonos_colaboracion.php
@@ -210,23 +210,28 @@ class ci_administrar_bonos_colaboracion extends mupum_ci
 			$total_por_consumir = $datos['monto_bono'];
 			///--$total_consumido = dao::get_total_consumido_en_bono_por_convenio_por_socio($datos['idafiliacion'],$datos['idconvenio']);
 			//--$maximo_por_convenio = dao::get_monto_maximo_mensual_convenio($datos['idconvenio']); 
-			
-			$periodo = dao::sacar_periodo_fecha($datos['fecha_compra']);
-			$estado_situacion = dao::get_total_estado_situacion($periodo,$datos['idafiliacion']);
-			$configuracion = dao::get_configuracion();
-			$limite_socio = $configuracion['limite_por_socio'];
+			if (isset($datos['idafiliacion']))
+			{	
+				$periodo = dao::sacar_periodo_fecha($datos['fecha_compra']);
+				$estado_situacion = dao::get_total_estado_situacion($periodo,$datos['idafiliacion']);
+				$configuracion = dao::get_configuracion();
+				$limite_socio = $configuracion['limite_por_socio'];
 
 
-			//--$total = $total_por_consumir + $total_consumido;
+				//--$total = $total_por_consumir + $total_consumido;
 
-			$estado_total = $estado_situacion + $total_por_consumir;  
-			if ($estado_total <= $limite_socio)
-			{
-				$this->cn()->set_dt_talonario_nros_bono_colaboracion($datos);
+				$estado_total = $estado_situacion + $total_por_consumir;  
+				if ($estado_total <= $limite_socio)
+				{
+					$this->cn()->set_dt_talonario_nros_bono_colaboracion($datos);
+				} else {
+
+					toba::notificacion()->agregar("El afiliado lleva consumido en este periodo : $".$estado_situacion. ", mas el valor del bono : $".round($total_por_consumir,2). " .Supera el limite maximo permitido por periodo por socio en la mutual de : $" .$limite_socio ,'info');
+				}
 			} else {
-
-				toba::notificacion()->agregar("El afiliado lleva consumido en este periodo : $".$estado_situacion. ", mas el valor del bono : $".round($total_por_consumir,2). " .Supera el limite maximo permitido por periodo por socio en la mutual de : $" .$limite_socio ,'info');
+				$this->cn()->set_dt_talonario_nros_bono_colaboracion($datos);
 			}
+			
 			
 		}
 	}
