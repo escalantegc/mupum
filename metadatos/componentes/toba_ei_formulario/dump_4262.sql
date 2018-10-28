cc66a5e00bbc0629df6c53c93ae0f309eb25bcef
Gustavo Escalante <escalantegc@gmail.com>
2018-10-28 19:14:06 -0300

    a

--- metadatos/componentes/toba_ei_formulario/dump_4262.sql
+++ metadatos/componentes/toba_ei_formulario/dump_4262.sql
@@ -1741,7 +1741,7 @@ INSERT INTO apex_objeto_ei_formulario_ef (objeto_ei_formulario_fila, objeto_ei_f
 	'foto', --identificador
 	'ef_upload', --elemento_formulario
 	'foto', --columnas
-	'1', --obligatorio
+	'0', --obligatorio
 	'0', --oculto_relaja_obligatorio
 	'20', --orden
 	'Foto', --etiqueta
